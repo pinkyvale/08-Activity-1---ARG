576c5b3aede2ab46609d4da275f87c8063098c27
pinkeu <vale.323532@pasay-edsa.sti.edu.ph>
2024-12-02 12:06:03 +0800

    Add files via upload

---
+++ One.java
@@ -0,0 +1,15 @@
+public class One {
+    
+    public interface Carnivore {
+    int pieces = 10;
+
+    void eatGrass(); 
+
+    default int chew() { 
+        return 13;
+    }
+}
+    public static void main (String[] args) {
+    }
+}
+
